51f6c502f11d6e9298b87a4ad23615fc5580762b
Hebom <aplilis@naver.com>
2023-06-21 11:39:01 +0900

    updated

--- src/controllers/videoController.js
+++ src/controllers/videoController.js
@@ -76,11 +76,13 @@ export const deleteVideo = async (req, res) => {
 
 export const search = async (req, res) => {
   const { keyword } = req.query;
+  let videos = [];
   if (keyword) {
-    const videos = await Video.find({
-      title: keyword,
+    videos = await Video.find({
+      title: {
+        $regex: new RegExp(keyword, "i"),
+      },
     });
-    return res.render("search", { pageTitle: "Search", videos });
   }
-  return res.render("search", { pageTitle: "Search" });
+  return res.render("search", { pageTitle: "Search", videos });
 };
