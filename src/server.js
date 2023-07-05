e043fe43e95b8e70d7f5a4ea28d26e45c3d8dd97
Hebom <aplilis@naver.com>
2023-07-05 14:17:56 +0900

    update

--- src/server.js
+++ src/server.js
@@ -18,10 +18,13 @@ app.use(express.urlencoded({ extended: true }));
 //세션 미들웨어를 추가했다.
 app.use(
   session({
-    secret: "Hello!",
-    resave: true,
-    saveUninitialized: true,
-    store: MongoStore.create({ mongoUrl: "mongodb://127.0.0.1:27017/wetube" }),
+    secret: process.env.COOKIE_SECRET,
+    resave: false,
+    saveUninitialized: false,
+    // cookie: {
+    //   maxAge: 20000,
+    // },
+    store: MongoStore.create({ mongoUrl: process.env.DB_URL }),
   })
 );
 
